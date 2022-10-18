b2807e4a408a50733b5ed8820aa87937d84ee12b
ttom1oka <aleksandrgarmashov06@gmail.com>
2022-10-18 17:49:37 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 
 	<?php include "left-sidebar.php"; ?>
-	<div class="content">тело</div>
+	<div class="content">ТЕЛО</div>
 	<?php  include "right-sidebar.php" ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <?php $i = 1; ?>
