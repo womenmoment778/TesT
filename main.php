dc867ce470e63412daed1d7fd384d7339944e48d
Aлександр <116090425+ttom1oka@users.noreply.github.com>
2022-10-20 15:27:16 +0300

    ааа таблички

--- main.php
+++ main.php
@@ -1,25 +1,26 @@
-<div class="container">
+
+<?php get_sidebar('left');?>
+	<div class="container">
   <div class="row">
-<div class="col">
+<div class="col-3">
       Колонка
     </div> 
-    <div class="col">
+    <div class="col-6">
       Колонка
     </div>
-    <div class="col">
+    <div class="col-3">
       Колонка
     </div>
   </div>
 </div>
-<?php get_sidebar('left');?>
-	<div class="content">ТЕЛО</div>
+  <!-- <div class="content">ТЕЛО</div> -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<?php $i = 1; ?>
+<!-- <?php $i = 1; ?>
 <?php while($i <= 20) { ?>
  <div>
 	<h1>заголовок нашей записи №<?php echo $i;?></h1>
 	<div>Краткое описание нашей записи №<?php echo $i;?></div>
 	<div><a href="/">Читать далее...</a></div>
 </div> 
-<?php $i++; }?>
+<?php $i++; }?> -->
 <?php get_sidebar('right');?>
