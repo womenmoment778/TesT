bb617437ee7468f36c328de31fc7b03a786b13dd
womenmoment778 <lqnaft.1991@mail.ru>
2022-10-24 17:59:12 +0300

    Обнова Хедера и Стиля

--- header.php
+++ header.php
@@ -1,1 +1,16 @@
-Header
+<div align="center" style="float: left; width: 60%;">
+	
+	<img src="https://html5css.ru/css/paris.jpg" alt="Avatar" class="avatar1"> Аватарка
+	<br>
+	<img src="https://html5css.ru/css/paris.jpg" alt="Avatar" class="avatar1"> Аватарка
+
+</div>
+<div align="center" style="float:left; width: -1%" >
+
+	<img src="https://html5css.ru/css/paris.jpg" alt="Avatar" class="avatar1">Аватарка
+	<br>
+	<img src="https://html5css.ru/css/paris.jpg" alt="Avatar" class="avatar1">Аватарка
+
+
+	</div>
+	<div style="clear:both"></div>
