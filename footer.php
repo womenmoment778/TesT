beec4c29b13397d15c39538f45112b24ae9e2697
womenmoment778 <lqnaft.1991@mail.ru>
2022-10-24 15:28:57 +0300

    Обнова

--- footer.php
+++ footer.php
@@ -1,14 +1,35 @@
+ <div style="float: left; width: 10% " >
 
- Обратная связь: <br>
+ Обратная связь:
+
+</div>
+ <div style="float: left; width: 15% " >
+
  Александр - pochta@mail.com <br>
- 	<a href=" " target="_blank"> - ссылка на вк.com</a> <br>
+    <a href=" " target="_blank"> - ссылка на вк.com</a> <br>
+
+</div>
+ <div style="float: left; width: 15% " >
+
  Сергей 1 - nosoksydby03@gmail.com <br>
- 	<a href=" " target="_blank">>aссылка на вк.com </a><br>
- Сергей 2 - lqnaft.1991@mail.ru <br>
- 	<a href="https://vk.com/id387650595" target="_blank"> ссылка на вк.com</a> <br>
+    <a href=" " target="_blank">>aссылка на вк.com </a><br>
+
+ </div>
+  <div style="float: left; width: 15% " >
+
+Панфилов Сергей - lqnaft.1991@mail.ru <br>
+    <a href="https://vk.com/id387650595" target="_blank"> ссылка на вк.com</a> <br>
+
+ </div>
+  <div style="float: left; width: 15% " >
+
  Юлия - Yluapet543@gmail.com <br>
- 	<a href=" " target="_blank">>ссылка на вк.com</a> <br>
- 	Сообщество: <br>
- 	<a href=" " target="_blank">Дискорд - ссылка на него.com </a>
+    <a href=" " target="_blank">>ссылка на вк.com</a> <br>
+
+ </div >
+
+    Сообщество: <br>
+    <a href=" " target="_blank">Дискорд - ссылка на него.com </a>
+
 
 </html>
